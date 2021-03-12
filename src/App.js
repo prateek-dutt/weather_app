9aa79fd5ef0fc75391dbf3bf3b0dafc9cb0de4e1
prateek-dutt <duttprateek@gmail.com>
2021-03-12 19:48:48 +0530

    Intitail Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import './App.css';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      "temp":'',
+      "temp_min":'',
+      "temp_max":''
+    };
+
+    this.Btn_Click=this.Btn_Click.bind(this);
+
+  }
+
+  Btn_Click(self){
+    console.log("I am clicking the button");
+  }
+
+
+
+  render(){
+    return(
+      <div class="main_grid"> 
+      <div class="main_box" class="container">
+        
+        <div class="input-field row s4">
+          <input id="city_name" type="text" class="validate" />
+          <label for="city_name">Enter the name of the city</label>
+        </div>
+        <button class="waves-effect  btn-small" onClick={this.Btn_Click}>Search</button>
+      </div>
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
